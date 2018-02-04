a73ca0fbfa296bd8f6bc79656f3f36dd40bb0214
Jiulong Hu <me@hujiulong.com>
2018-02-05 01:42:32 +0800

    chore: init.

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import pkg from './package.json'
+import babel from 'rollup-plugin-babel';
+
+const banner = `/* @preserve
+ * gcoord ${pkg.version}, a JS library for transform geographical coordinates.
+ * Copyright (c) 2018 Jiulong Hu <me@hujiulong.com>
+ */
+`;
+
+export default {
+	input: 'src/index.js',
+	output: [
+		{
+			file: pkg.main,
+			format: 'umd',
+			name: 'gcoord',
+			banner: banner,
+			sourcemap: true
+		},
+		{
+			file: pkg.module,
+			format: 'es',
+			banner: banner,
+			sourcemap: true
+		}
+	],
+	legacy: true, // Needed to create files loadable by IE8
+    plugins: [
+        babel()
+    ]
+};
