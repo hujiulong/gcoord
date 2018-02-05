80ff937bea3162356389da483c99327da12ac9d6
Jiulong Hu <me@hujiulong.com>
2018-02-05 22:14:56 +0800

    fix: fix circular dependency

--- src/crs/BD09.js
+++ src/crs/BD09.js
@@ -1,11 +1,8 @@
-import GCJ02 from './GCJ02.js'
-import { compose } from '../helper.js'
-
 const { sin, cos, atan2, sqrt, abs, PI } = Math;
 
 const baiduFactor = PI * 3000.0 / 180.0;
 
-function toGCJ02( coord ) {
+export function BD09toGCJ02( coord ) {
 
     const [ lon, lat ] = coord;
 
@@ -13,13 +10,13 @@ function toGCJ02( coord ) {
     const y = lat - 0.006;
     const z = sqrt( x * x + y * y ) - 0.00002 * sin( y * baiduFactor );
     const theta = atan2( y, x ) - 0.000003 * cos( x * baiduFactor );
-    const newX = z * cos( theta );
-    const newY = z * sin( theta );
+    const newLon = z * cos( theta );
+    const newLat = z * sin( theta );
 
     return [ newLon, newLat ];
 }
 
-function fromGCJ02( coord ) {
+export function GCJ02toBD09( coord ) {
 
     const [ lon, lat ] = coord;
 
@@ -33,15 +30,4 @@ function fromGCJ02( coord ) {
 
     return [ newLon, newLat ];
 
-}
-
-export default {
-    from: {
-        WGS84: compose( fromGCJ02, GCJ02.from.WGS84 ),
-        GCJ02: fromGCJ02,
-    },
-    to: {
-        WGS84: compose( GCJ02.to.WGS84, toGCJ02 ),
-        GCJ02: toGCJ02,
-    }
 }
--- src/crs/index.js
+++ src/crs/index.js
@@ -1,9 +1,30 @@
-import WGS84 from './WGS84.js'
-import GCJ02 from './GCJ02.js'
-import BD09 from './BD09.js'
+import { GCJ02toWGS84, WGS84toGCJ02 } from './GCJ02.js'
+import { BD09toGCJ02, GCJ02toBD09 } from './BD09.js'
+import { compose } from '../helper.js'
 
-export default {
+const WGS84 = {
+    to: {
+        GCJ02: WGS84toGCJ02,
+        BD09: compose( GCJ02toBD09, WGS84toGCJ02 )
+    }
+}
+
+const GCJ02 = {
+    to: {
+        WGS84: GCJ02toWGS84,
+        BD09: GCJ02toBD09
+    }
+}
+
+const BD09 = {
+    to: {
+        WGS84: compose( GCJ02toWGS84, BD09toGCJ02 ),
+        GCJ02: BD09toGCJ02
+    }
+}
+
+export {
     WGS84,
     GCJ02,
-    BD09,
-};
+    BD09
+}
