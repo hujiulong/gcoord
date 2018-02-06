200999757112f72d188981c13058e4a7a17ff086
Jiulong Hu <me@hujiulong.com>
2018-02-06 22:38:09 +0800

    feat: mixing named and default exports

--- src/index.js
+++ src/index.js
@@ -1,3 +1,61 @@
-export * from './constants.js'
-export * from './transform.js'
-export * from './geojson.js'
+import { WGS84, WGS1984, GCJ02, BD09, EPSG4326 } from './constants.js'
+import {
+    feature,
+    geometry,
+    point,
+    points,
+    polygon,
+    polygons,
+    lineString,
+    lineStrings,
+    featureCollection,
+    multiLineString,
+    multiPoint,
+    multiPolygon,
+    geometryCollection
+} from './geojson.js'
+import { transform } from './transform.js'
+
+export {
+    WGS84,
+    WGS1984,
+    GCJ02,
+    BD09,
+    EPSG4326,
+    transform,
+    feature,
+    geometry,
+    point,
+    points,
+    polygon,
+    polygons,
+    lineString,
+    lineStrings,
+    featureCollection,
+    multiLineString,
+    multiPoint,
+    multiPolygon,
+    geometryCollection
+}
+
+export default {
+    WGS84,
+    WGS1984,
+    GCJ02,
+    BD09,
+    EPSG4326,
+    transform,
+    feature,
+    geometry,
+    point,
+    points,
+    polygon,
+    polygons,
+    lineString,
+    lineStrings,
+    featureCollection,
+    multiLineString,
+    multiPoint,
+    multiPolygon,
+    geometryCollection
+}
