b8dd2ee1c12123f1a42fc4921108552f8537d003
Jiulong Hu <me@hujiulong.com>
2018-03-03 11:29:53 +0800

    style: fix names of CRS functions

--- src/crs/index.js
+++ src/crs/index.js
@@ -1,25 +1,25 @@
-import { GCJ02toWGS84, WGS84toGCJ02 } from './GCJ02.js'
-import { BD09toGCJ02, GCJ02toBD09 } from './BD09.js'
+import { GCJ02ToWGS84, WGS84ToGCJ02 } from './GCJ02.js'
+import { BD09ToGCJ02, GCJ02ToBD09 } from './BD09.js'
 import { compose } from '../helper.js'
 
 const WGS84 = {
     to: {
-        GCJ02: WGS84toGCJ02,
-        BD09: compose( GCJ02toBD09, WGS84toGCJ02 )
+        GCJ02: WGS84ToGCJ02,
+        BD09: compose( GCJ02ToBD09, WGS84ToGCJ02 )
     }
 }
 
 const GCJ02 = {
     to: {
-        WGS84: GCJ02toWGS84,
-        BD09: GCJ02toBD09
+        WGS84: GCJ02ToWGS84,
+        BD09: GCJ02ToBD09
     }
 }
 
 const BD09 = {
     to: {
-        WGS84: compose( GCJ02toWGS84, BD09toGCJ02 ),
-        GCJ02: BD09toGCJ02
+        WGS84: compose( GCJ02ToWGS84, BD09ToGCJ02 ),
+        GCJ02: BD09ToGCJ02
     }
 }
 
