a094993e941cb508e6defaf0fd7e0079892f24d5
Jiulong Hu <me@hujiulong.com>
2018-03-25 03:49:20 +0800

    feat: support web mercator projection(#2)

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,12 @@ export const WGS84 = 'WGS84';
 export const WGS1984 = WGS84;
 export const GCJ02 = 'GCJ02';
 export const BD09 = 'BD09';
+export const WebMercator = 'WebMercator';
 
 // EPSG
 export const EPSG4326 = WGS84;
+export const EPSG3857 = WebMercator;
+
+export const Baidu = BD09;
+export const BMap = BD09;
+export const AMap = GCJ02;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,2 @@
-import { WGS84, WGS1984, GCJ02, BD09, EPSG4326 } from './constants.js'
-import { transform } from './transform.js'
-
-export {
-    WGS84,
-    WGS1984,
-    GCJ02,
-    BD09,
-    EPSG4326,
-    transform,
-}
-
-export default {
-    WGS84,
-    WGS1984,
-    GCJ02,
-    BD09,
-    EPSG4326,
-    transform,
-}
+export * from './constants.js'
+export { transform } from './transform.js'
