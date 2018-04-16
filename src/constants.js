4272940ac717b260247a21980d3fcaa168380a22
Jiulong Hu <me@hujiulong.com>
2018-04-16 15:03:22 +0800

    fix: fix a bug throwing an exception when transforming the EPSG3857(#3)

--- src/constants.js
+++ src/constants.js
@@ -2,13 +2,12 @@ export const WGS84 = 'WGS84';
 export const WGS1984 = WGS84;
 export const GCJ02 = 'GCJ02';
 export const BD09 = 'BD09';
-export const WebMercator = 'WebMercator';
-
-// EPSG
 export const EPSG4326 = WGS84;
-export const EPSG3857 = WebMercator;
-export const EPSG900913 = WebMercator;
+export const EPSG3857 = 'EPSG3857';
+export const EPSG900913 = EPSG3857;
 
 export const Baidu = BD09;
 export const BMap = BD09;
 export const AMap = GCJ02;
+
+export const WebMercator = EPSG3857;
