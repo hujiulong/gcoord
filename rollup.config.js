102fb4277d432d4285547f365e53d95d1949abf3
Jiulong Hu <me@hujiulong.com>
2018-04-29 01:28:46 +0800

    feat: mixing named and default exports

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import pkg from './package.json'
 import babel from 'rollup-plugin-babel';
 
 const banner = `/* @preserve
- * gcoord ${pkg.version}, a JS library for transform geographical coordinates.
+ * gcoord ${pkg.version}, ${pkg.description}
  * Copyright (c) 2018 Jiulong Hu <me@hujiulong.com>
  */
 `;
@@ -15,14 +15,12 @@ export default {
             format: 'umd',
             name: pkg.name,
             banner: banner,
-            exports: 'named',
             sourcemap: true
         },
         {
             file: pkg.module,
             format: 'es',
             banner: banner,
-            exports: 'named',
             sourcemap: true
         }
     ],
--- src/index.js
+++ src/index.js
@@ -1,2 +1,47 @@
-export * from './constants.js'
-export { transform } from './transform.js'
+import {
+    WGS84,
+    WGS1984,
+    GCJ02,
+    BD09,
+    EPSG4326,
+    EPSG3857,
+    EPSG900913,
+    Baidu,
+    BMap,
+    AMap,
+    WebMercator
+} from './constants.js'
+
+import { transform } from './transform.js'
+
+export {
+    WGS84,
+    WGS1984,
+    GCJ02,
+    BD09,
+    EPSG4326,
+    EPSG3857,
+    EPSG900913,
+    Baidu,
+    BMap,
+    AMap,
+    WebMercator,
+
+    transform
+}
+
+export default {
+    WGS84,
+    WGS1984,
+    GCJ02,
+    BD09,
+    EPSG4326,
+    EPSG3857,
+    EPSG900913,
+    Baidu,
+    BMap,
+    AMap,
+    WebMercator,
+
+    transform
+}
