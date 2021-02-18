fb2a4649904dec1f6bc0d29d0113b273625de44a
Jiulong Hu <me@hujiulong.com>
2021-02-18 11:07:26 +0800

    workflow: adjust eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,17 @@
 module.exports = {
-  extends: ['airbnb-typescript/base'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
   rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
     'no-continue': 'off',
     'no-mixed-operators': 'off',
     'no-plusplus': 'off',
