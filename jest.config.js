cb48d77ea134e3bda75814600e53476ee7e2c0e8
Jiulong Hu <me@hujiulong.com>
2021-02-18 11:16:41 +0800

    chore: update deps

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
   testURL: 'http://localhost/',
   globals: {
     'ts-jest': {
-      tsConfig: 'tsconfig.json',
+      tsconfig: 'tsconfig.json',
       isolatedModules: true
     }
   },
