b62a3b96ddd897ca067ed51f7fd117c0268dded3
Jiulong Hu <me@hujiulong.com>
2024-03-12 17:54:44 +0800

    fix: support for mini-program (closes #232)

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-if (process.env.NODE_ENV === 'production') {
+if (typeof process === 'undefined' || process.env.NODE_ENV === 'production') {
   module.exports = require('./dist/gcoord.cjs.prod.js');
 } else {
   module.exports = require('./dist/gcoord.cjs.js');
